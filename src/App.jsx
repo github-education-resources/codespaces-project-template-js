de9b0a4192f9431e8a106c067fa09c644bad8eb4
Jx <121834179+Jhesneydy@users.noreply.github.com>
2023-11-10 14:19:49 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Jhesmidy",
+  title: "englsih teacher",
+  email: "la_teacherx@example.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "la teacher",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
