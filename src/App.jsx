931eaa24dbfe9e4eb0fca29178808b0f08260c03
FranMariotti <149805640+FranMariotti@users.noreply.github.com>
2024-03-26 19:31:20 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "aaaa Grenier",
   title: "Web Designer & Content Creator",
   email: "alex@example.com",
   gitHub: "microsoft",
