e5b36105bdc82c6dd9e993962690e7f382d4e773
Tim-g-t <120601407+Tim-g-t@users.noreply.github.com>
2024-03-26 20:02:58 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  name: "Tim Töpper",
+  title: "AI student",
+  email: "tim.toepper@studentambassadores.com",
   gitHub: "microsoft",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "tim",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
