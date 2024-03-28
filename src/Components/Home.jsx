ed76009047a7738ece721432d728809dc68f5e55
Tim-g-t <120601407+Tim-g-t@users.noreply.github.com>
2024-03-28 12:28:26 +0100

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/IMG-20221013-WA0020-fotor-2024030414737.png";
+import image from "../../images/IMG-20221013-WA0020-fotor-2024030414737.png";
 
 const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
 
