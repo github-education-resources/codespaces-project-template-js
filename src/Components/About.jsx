ca3e7529f09568052061e7c02b58113b4230feaa
Tim-g-t <120601407+Tim-g-t@users.noreply.github.com>
2024-03-28 12:27:31 +0100

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,16 +24,16 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a AI/UX student studying at Frankfurt School University.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
+  "Ai",
   "User experience",
-  "Inclusive design",
+  "Python",
   "Focus group testing",
   "Mobile user interfaces",
   "Graphic design",
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my AI/UX experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
