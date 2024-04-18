931de9e5e94edc82a47320bc7f5e71102a40d061
Muhammad Usman Ali <117012473+dotusmanali@users.noreply.github.com>
2024-04-18 13:04:51 +0500

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Muhammad Usman Ali",
+  title: "Blockchain Developer",
+  email: "Muhammad.UsmanAli@studentambassadors.com",
+  gitHub: "dotusmanali",
+  instagram: "dotusmanali",
+  linkedIn: "dotusmanali",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "dotusmanali",
+  youTube: "dotusmanali",
 };
 
 const primaryColor = "#4E567E";
