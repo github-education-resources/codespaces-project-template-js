33b1ee6bf93331835c1ac0e8564380f759250a1a
Muhammad Usman Ali <117012473+dotusmanali@users.noreply.github.com>
2024-04-18 15:21:15 +0500

    add changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "CrownLink",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Decentralized CrowdFunding Platform",
+    url: "https://github.com/dotusmanali/CrowdLink",
   },
   {
-    title: "Web Development for Beginners",
+    title: "next email",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "next email",
+    url: "https://github.com/dotusmanali/next-email",
   },
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "My Resume Site",
+    url: "https://dotusmanali.github.io/usman",
   },
 ];
 
