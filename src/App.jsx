aba5a8071eecf01b2833c69d210c770687bf51ea
vic-sierrxa <144049689+vic-sierrxa@users.noreply.github.com>
2024-06-15 14:11:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Vyomkesh Sharma",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
+  email: "avyomkeshsharma9231@gmail.com",
   gitHub: "microsoft",
   instagram: "microsoft",
   linkedIn: "satyanadella",
