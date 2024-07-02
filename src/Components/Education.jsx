802910d46272af9046d76226214f74a3d8173833
Sayan Biswas <96792157+sayanbiswas0408@users.noreply.github.com>
2024-07-02 12:56:38 +0000

    final deployment

---
+++ src/Components/Education.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Education = () => {
+    return(
+        <section className="light" id="education">
+            <h2>Submitted by Sayan</h2>
+        </section>
+    )
+}
+
+export default Education;
