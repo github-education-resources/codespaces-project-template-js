29d83b4fb8931040bde16fc29df6e9296ee61020
BalasriMukkala <162893974+BalasriMukkala@users.noreply.github.com>
2024-09-04 22:54:23 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/server-wall.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -24,19 +24,16 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
-
+"I'm an aspiring Frontend Developer have proficiency in HTML, CSS, Javascript and I am passionate about developing user-interactive websites.";
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Frontend Development",
+  "JavaScript",
+  "HTML/CSS",
+  "Bootstrap",
 ];
 
 /**
@@ -45,7 +42,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my web designing experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
