f6c779b54935e7668727e7d76788e157788d7a92
BalasriMukkala <162893974+BalasriMukkala@users.noreply.github.com>
2024-09-04 22:45:19 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/server-wall.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
